09bcf986b3a66f3eec795fbd7e9f9ecf47388393
李华升 <252373893@qq.com>
2022-05-20 16:18:28 +0800

    init

--- src/views/MarkdownEditor.vue
+++ src/views/MarkdownEditor.vue
@@ -2,16 +2,27 @@
   <div>
     <div class="container">
       <MavonEditor @change-content="receive" default='## Hello'/>
+      <el-divider/>
+      <div class="preview" v-html="mdHtml" />
     </div>
   </div>
 </template>
 
 <script setup>
 import MavonEditor from '@/components/editors/MavonEditor.vue'
+import { mavonEditor } from 'mavon-editor'
+import {computed, ref} from "vue";
 
+const markdownIt = mavonEditor.getMarkdownIt()
+const md = ref('');
 const receive = (content) => {
-  console.log(content)
+  md.value = content;
 }
+
+const mdHtml = computed(() => {
+  return markdownIt.render(md.value)
+})
+
 
 </script>
 
