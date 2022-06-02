581b3348f2ea2edb064dded77d32a6b99a1e5714
李华升 <252373893@qq.com>
2022-06-02 11:02:46 +0800

    init

--- src/components/editors/MonacoEditor.vue
+++ src/components/editors/MonacoEditor.vue
@@ -1,75 +1,86 @@
 <template>
   <div>
-    <div class="border" id="editor" ref="editor"></div>
+    <div className="border" id="editor" ref="editor"></div>
   </div>
 </template>
 
-<script>
+<script setup>
 import * as monaco from "monaco-editor";
 import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import JsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 import CssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import TsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import {onMounted, ref} from "vue";
 
-export default {
-  props: {
-    default: {
-      type: String,
-      default: ""
-    },
+const props = defineProps({
+  default: {
+    type: String,
   },
-  data: () => ({}),
-  methods: {},
-  mounted() {
-    self.MonacoEnvironment = {
-      getWorker(_, label) {
-        if (label === "json") {
-          return new JsonWorker();
-        }
-        if (label === "css" || label === "scss" || label === "less") {
-          return new CssWorker();
-        }
-        if (label === "html" || label === "handlebars" || label === "razor") {
-          return new HtmlWorker();
-        }
-        if (label === "typescript" || label === "javascript") {
-          return new TsWorker();
-        }
-        return new EditorWorker();
-      },
-    };
-    const editor = monaco.editor.create(this.$refs.editor, {
-      language: "json",
-      lineNumbers: "on",
-      minimap: {
-        enabled: false
-      },
-      overviewRulerBorder: false,
-      renderLineHighlight: 'none',
-      scrollBeyondLastLine: false,
-      automaticLayout: true,
-      fontSize: "14px",
-      lineNumbersMinChars: 3,
-      overviewRulerLanes: 0,
-      hideCursorInOverviewRuler: true,
-      find: {
-        addExtraSpaceOnTop: false,
-      },
-      formatOnType: true,
-      formatOnPaste: true,
-      scrollbar: {
-        horizontal: "hidden",
-        vertical: "hidden",
+});
+
+let editor = ref(null);
+
+let emit = defineEmits(["change-content"]);
+
+onMounted(() => {
+  window.MonacoEnvironment = {
+    getWorker(_, label) {
+      if (label === "json") {
+        return new JsonWorker();
       }
-    });
-    editor.setValue(this.default);
-    editor.onDidChangeModelContent(() => {
-      //编辑器内容change事件
-      this.$emit("change-content", editor.getValue());
-    });
-  },
-};
+      if (label === "css" || label === "scss" || label === "less") {
+        return new CssWorker();
+      }
+      if (label === "html" || label === "handlebars" || label === "razor") {
+        return new HtmlWorker();
+      }
+      if (label === "typescript" || label === "javascript") {
+        return new TsWorker();
+      }
+      return new EditorWorker();
+    },
+  };
+
+  editor = monaco.editor.create(document.getElementById('editor'), {
+    language: "json",
+    lineNumbers: "on",
+    minimap: {
+      enabled: false
+    },
+    overviewRulerBorder: false,
+    renderLineHighlight: 'none',
+    scrollBeyondLastLine: false,
+    automaticLayout: true,
+    fontSize: "14px",
+    lineNumbersMinChars: 3,
+    overviewRulerLanes: 0,
+    hideCursorInOverviewRuler: true,
+    find: {
+      addExtraSpaceOnTop: false,
+    },
+    formatOnType: true,
+    formatOnPaste: true,
+    scrollbar: {
+      horizontal: "hidden",
+      vertical: "hidden",
+    }
+  });
+  editor.setValue(props.default);
+  editor.onDidChangeModelContent(() => {
+    //编辑器内容change事件
+    emit("change-content", editor.getValue());
+  });
+})
+
+const formatJson = () =>{
+  editor.getAction('editor.action.formatDocument').run();
+}
+
+defineExpose({
+  formatJson,
+});
+
 </script>
 
 <style scoped>
--- src/views/JsonEditor.vue
+++ src/views/JsonEditor.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <div class="container">
-      <MonacoEditor @change-content="receive" default='{}'/>
+      <el-button type="primary" @click="format" style="margin-bottom: 20px">格式化</el-button>
+      <MonacoEditor @change-content="receive" default='{}' ref="monacoRef"/>
       <el-divider/>
       <div>{{ result }}</div>
     </div>
@@ -13,11 +14,17 @@
 import MonacoEditor from '@/components/editors/MonacoEditor.vue';
 import {ref} from "vue";
 
+const monacoRef = ref(null)
 const result = ref('')
 
 const receive = (value) => {
   result.value = value
 }
+
+const format = () => {
+  monacoRef.value.formatJson();
+}
+
 </script>
 
 <style scoped>
